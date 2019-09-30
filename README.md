53493598679ae4448548a5ae00c50690ac60b6db
gcrawf <29676221+gcrawf@users.noreply.github.com>
2019-09-30 19:42:36 +0100

    Add first version of choisir.sh; may need to be updated further to support input from stdin.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # utils
 A collection of small (and possibly pointless) scripts I wrote to speed up repetitive tasks.
+
+## `choisir.sh`
+A short script designed to aid in creating more user-friendly dmenu prompts.
+Parses a series of tab separated pairs from a file, prompts the user with the keys and prints the respective value to stdout.
+Calling the script with the flag `--help` gives a brief help message describing its function.
+(At present input cannot be read from stdin; will be improved/reworked in the next update.)
+
+**Usage**: `./choisir.sh (LOCATION|--help)`
---
+++ choisir.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# Ensure that an argument is passed to the script
+if [ -n "$1" -o "$1" = "-" ]
+then
+	if [ "$1" != "--help" ]
+	then
+		# Get the first element of each valid pair (i.e. key<TAB>value)
+		location="$1"
+		choices=`grep -P "\t" "$location" | cut -f 1`
+
+		# Print the result for the user's choice from the TSV file.
+		key=`echo "$choices" | dmenu`
+		value=`grep -P "$key\t" "$location" | cut -f 2`
+		echo "$value"
+	else
+		# Print help message
+		echo "choisir: A small script to speed up work with dmenu."
+		echo "Usage: choisir (LOCATION|--help)"
+		
+		echo ""
+
+		echo "choisir takes a series of tab-separated pairs from a file specified by the user."
+		echo "The first item in each pair is passed to dmenu, and the respective second item is printed to stdout."
+		echo "(Currently input from stdin is not yet supported.)"
+
+		echo ""
+
+		echo "Flags:"
+		echo "	--help: prints this help message"
+	fi
+else
+	# Tell user to pass a command
+	echo "Usage: choisir LOCATION"
+	echo "Type choisir --help for more information"
+	echo "(Currently stdin is not accepted as a valid input location.)"
+fi
